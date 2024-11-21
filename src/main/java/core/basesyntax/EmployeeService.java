94cbc78e28b5534a11bc6a04c9028b5687bc9015
Viktoriia <shmandiuk.grizzly@gmail.com>
2024-11-21 19:16:57 +0100

    create a compator and implement the nethod

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,29 @@
 package core.basesyntax;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EmployeeService {
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        if (employees == null) {
+            throw new IllegalArgumentException("Input list cannot be null");
+        }
+        Comparator<Employee> employeeComparator = new Comparator<>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                int ageCompared = Integer.compare(o1.getAge(), o2.getAge());
+                if (ageCompared != 0) {
+                    return ageCompared;
+                }
+                return o1.getName().compareToIgnoreCase(o2.getName());
+            }
+        };
+
+        Set<Employee> sortedEmployees = new TreeSet<>(employeeComparator);
+        sortedEmployees.addAll(employees);
+
+        return sortedEmployees;
     }
 }
